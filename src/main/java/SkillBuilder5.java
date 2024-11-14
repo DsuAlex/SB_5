de9f7c1bf3994273544516439a304ee1fffd321c
DsuAlex <156232714+DsuAlex@users.noreply.github.com>
2024-11-14 16:31:54 -0500

    update

--- src/main/java/SkillBuilder5.java
+++ src/main/java/SkillBuilder5.java
@@ -3,8 +3,52 @@ import java.util.Random;
 public class SkillBuilder5 {
     private static Random rand = new Random(17);
 
-    public static boolean playCraps(){
-        //TODO: replace this line with your code.
+    public static boolean playCraps() {
+        boolean isWon = false;
+        int diceOne = rand.nextInt(6);
+        int diceTwo = rand.nextInt(6);
+        diceOne += 1;
+        diceTwo += 1;
+        int i = 1;
+        int roll = diceOne + diceTwo;
+
+        if (i == 1) {
+            if (roll == 7 || roll == 11) {
+                isWon = true;
+            } else if (roll == 2 || roll == 3 || roll == 12) {
+                isWon = false;
+
+            } else {
+                i++;
+
+            }
+        } else if (i > 1) {
+            int initialRoll = roll;
+            boolean isRolling = true;
+            while (isRolling) {
+                diceOne = rand.nextInt(6);
+                diceOne += 1;
+                diceTwo = rand.nextInt(6);
+                diceTwo += 1;
+                roll = diceOne + diceTwo;
+                if (roll == initialRoll) {
+                    isWon = true;
+                    isRolling = false;
+
+                } else if (roll == 7 || roll == 11) {
+                    isWon = false;
+                    isRolling = false;
+
+
+                } else {i++;}
+
+
+            }
+
+
+        }
+        return isWon;
+
     }
 
     private static void generateLevel(int level, int[] a){
@@ -12,10 +56,10 @@ public class SkillBuilder5 {
     }
 
     private static String generateLevelString(int level, int[] a){
-        // TODO: replace this line with your code.
+        return null;
     }
 
     public static String pascalsTriangle(int numOfRows){
-        // TODO: replace this line with your code.
+       return null;
     }
 }
